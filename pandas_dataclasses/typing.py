406550269e6920232acfc20479288403d255f0e5
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-02-25 09:24:42 +0000

    #5 Add named collection for public type hints

--- pandas_dataclasses/typing.py
+++ pandas_dataclasses/typing.py
@@ -3,6 +3,25 @@ __all__ = []
 
 # standard library
 from enum import Enum
+from typing import Hashable, TypeVar
+from typing import Collection as Collection_
+
+
+# dependencies
+from typing_extensions import Protocol
+
+
+# type hints (private)
+Tdtype = TypeVar("Tdtype", covariant=True)
+Tname = TypeVar("Tname", bound=Hashable, covariant=True)
+
+
+class Named(Protocol[Tname]):
+    pass
+
+
+class Collection(Collection_[Tdtype], Named[Tname], Protocol):
+    pass
 
 
 # constants
