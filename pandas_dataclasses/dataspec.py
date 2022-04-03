aedc6902b241dc9ab2e2a2e0bf9d6896079db904
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-04-03 16:32:42 +0000

    #7 Add classmethod (from_dataclass)

--- pandas_dataclasses/dataspec.py
+++ pandas_dataclasses/dataspec.py
@@ -2,21 +2,22 @@ __all__ = ["DataSpec"]
 
 
 # standard library
-from dataclasses import Field, dataclass, field
-from typing import Any, Dict, Hashable, Optional, Union
+from dataclasses import Field, dataclass, field, fields
+from typing import Any, Dict, Hashable, Optional, Type, Union
 
 
 # dependencies
 import numpy as np
-from typing_extensions import Literal
+from typing_extensions import Literal, ParamSpec, get_type_hints
 
 
 # submodules
-from .typing import FieldType, deannotate, get_dtype, get_ftype, get_name
+from .typing import DataClass, FieldType, deannotate, get_dtype, get_ftype, get_name
 
 
 # type hints
 FieldSpec = Union["ArrayFieldSpec", "MetaFieldSpec"]
+PInit = ParamSpec("PInit")
 
 
 # runtime classes
@@ -101,8 +102,38 @@ class DataSpec:
     fields: Dict[str, FieldSpec] = field(default_factory=dict)
     """Field specifications of the dataclass."""
 
+    @classmethod
+    def from_dataclass(
+        cls,
+        dataclass: Type[DataClass[PInit]],
+        cache: bool = True,
+    ) -> "DataSpec":
+        """Create a specification from a dataclass."""
+        if cache and hasattr(dataclass, "__dataspec__"):
+            return dataclass.__dataspec__  # type: ignore
+
+        dataspec = cls()
+        eval_fields(dataclass)
+
+        for field in fields(dataclass):
+            fieldspec = get_fieldspec(field)
+
+            if fieldspec is not None:
+                dataspec.fields[field.name] = fieldspec
+
+        dataclass.__dataspec__ = dataspec  # type: ignore
+        return dataspec
+
 
 # runtime functions
+def eval_fields(dataclass: Type[DataClass[PInit]]) -> None:
+    """Evaluate types of dataclass fields."""
+    types = get_type_hints(dataclass, include_extras=True)
+
+    for field in fields(dataclass):
+        field.type = types[field.name]
+
+
 def get_fieldspec(field: "Field[Any]") -> Optional[FieldSpec]:
     """Parse a dataclass field and return a field specification."""
     ftype = get_ftype(field.type)
