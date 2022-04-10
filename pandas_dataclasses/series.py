2ba99f14a2c1b52f8a67933862e0500869668a08
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-04-10 02:37:13 +0000

    #13 Add runtime functions

--- pandas_dataclasses/series.py
+++ pandas_dataclasses/series.py
@@ -1,13 +1,18 @@
+__all__ = ["asseries"]
+
+
 # standard library
-from typing import List, Union
+from typing import Any, Dict, Hashable, List, Optional, Union
 
 
 # dependencies
+import numpy as np
 import pandas as pd
 from typing_extensions import ParamSpec, Protocol
 
 
 # submodules
+from .dataspec import DataSpec
 from .typing import DataClass
 
 
@@ -21,3 +26,76 @@ class DataClass(DataClass, Protocol[PInit]):
 
     def __init__(self, *args: PInit.args, **kwargs: PInit.kwargs) -> None:
         ...
+
+
+# runtime functions
+def asseries(obj: DataClass[PInit]) -> "pd.Series[Any]":
+    """Create a Series object from a pandas dataclass."""
+    series = pd.Series(
+        data=get_data(obj),
+        dtype=get_dtype(obj),
+        index=get_index(obj),  # type: ignore
+        name=get_name(obj),
+    )
+
+    series.attrs.update(get_attrs(obj))
+    return series
+
+
+def get_attrs(obj: DataClass[PInit]) -> Dict[Hashable, Any]:
+    """Return the attributes for a Series object."""
+    dataspec = DataSpec.from_dataclass(type(obj))
+    attrs: Dict[Hashable, Any] = {}
+
+    for key, spec in dataspec.fields.attr.items():
+        attrs[spec.name] = getattr(obj, key)
+
+    return attrs
+
+
+def get_data(obj: DataClass[PInit]) -> Optional[Any]:
+    """Return the data for a Series object."""
+    dataspec = DataSpec.from_dataclass(type(obj))
+
+    for key in dataspec.fields.data:
+        return getattr(obj, key)
+
+
+def get_dtype(obj: DataClass[PInit]) -> Optional["np.dtype[Any]"]:
+    """Return the data type (dtype) for a Series object."""
+    dataspec = DataSpec.from_dataclass(type(obj))
+
+    for spec in dataspec.fields.data.values():
+        return spec.data.type
+
+
+def get_index(obj: DataClass[PInit]) -> Optional[IndexLike]:
+    """Return the index(es) for a Series object."""
+    dataspec = DataSpec.from_dataclass(type(obj))
+    indexes: List[pd.Index] = []
+
+    for key, spec in dataspec.fields.index.items():
+        indexes.append(
+            pd.Index(
+                np.atleast_1d(getattr(obj, key)),
+                dtype=spec.data.type,
+                name=spec.name,
+            )
+        )
+
+    if len(indexes) > 1:
+        return indexes
+
+    if len(indexes) == 1:
+        return indexes[0]
+
+
+def get_name(obj: DataClass[PInit]) -> Optional[Hashable]:
+    """Return the name for a Series object."""
+    dataspec = DataSpec.from_dataclass(type(obj))
+
+    for key in dataspec.fields.name:
+        return getattr(obj, key)
+
+    for spec in dataspec.fields.data.values():
+        return spec.name
