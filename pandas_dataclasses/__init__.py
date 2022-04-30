17301ababed035bdde77e7c2a77845f5ad3a0c44
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-04-30 15:09:09 +0000

    #24 Update package root

--- pandas_dataclasses/__init__.py
+++ pandas_dataclasses/__init__.py
@@ -1,15 +1,17 @@
-# type: ignore
 __author__ = "Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>"
 __version__ = "0.1.0"
 
 
 # submodules
-from . import specs
-from . import typing
-from . import pandas
+from . import specs  # type: ignore
+from . import typing  # type: ignore
+
+
+# subpackages
+from . import pandas  # type: ignore
 
 
 # aliases
+from .pandas import *
 from .specs import *
 from .typing import *
-from .pandas.series import *
