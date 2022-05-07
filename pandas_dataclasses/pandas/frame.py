2be0a4e9d2978a783332a22b6b10216dfaf8b691
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-05-07 05:14:24 +0000

    #30 Add AsDataFrame

--- pandas_dataclasses/pandas/frame.py
+++ pandas_dataclasses/pandas/frame.py
@@ -1,8 +1,15 @@
-__all__ = ["asdataframe"]
+__all__ = ["AsDataFrame", "asdataframe"]
+
+
+# standard library
+from functools import wraps
+from types import MethodType
+from typing import Any, Callable, Type
 
 
 # dependencies
 import pandas as pd
+from morecopy import copy
 from typing_extensions import ParamSpec, Protocol
 
 
@@ -20,6 +27,42 @@ class DataClass(DataClass, Protocol[PInit]):
 
     def __init__(self, *args: PInit.args, **kwargs: PInit.kwargs) -> None:
         ...
+
+
+# runtime classes
+class classproperty:
+    """Class property only for AsDataFrame.new().
+
+    As ``classmethod`` and ``property`` can be chained since Python 3.9,
+    this will be removed when the support for Python 3.7 and 3.8 ends.
+
+    """
+
+    def __init__(self, func: Any) -> None:
+        self.__func__ = func
+
+    def __get__(
+        self,
+        obj: Any,
+        cls: Type[DataClass[PInit]],
+    ) -> Callable[PInit, pd.DataFrame]:
+        return self.__func__(cls)
+
+
+class AsDataFrame:
+    """Mix-in class that provides shorthand methods."""
+
+    @classproperty
+    def new(cls) -> Any:
+        """Create a DataFrame object from dataclass parameters."""
+        init = copy(cls.__init__)
+        init.__annotations__["return"] = pd.DataFrame
+
+        @wraps(init)
+        def new(cls: Any, *args: Any, **kwargs: Any) -> Any:
+            return asdataframe(cls(*args, **kwargs))
+
+        return MethodType(new, cls)
 
 
 # runtime functions
