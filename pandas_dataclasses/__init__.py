98fc51506afb10e6cd1c58b75692502c2fc3f4ee
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-05-14 15:43:54 +0000

    #40 Update root of (sub)package

--- pandas_dataclasses/__init__.py
+++ pandas_dataclasses/__init__.py
@@ -2,17 +2,10 @@ __version__ = "0.2.0"
 
 
 # submodules
-from . import core  # type: ignore
-from . import specs  # type: ignore
-from . import typing  # type: ignore
+from .core import *
+from .specs import *
+from .typing import *
 
 
 # subpackages
-from . import pandas  # type: ignore
-
-
-# aliases
-from .core import *
 from .pandas import *
-from .specs import *
-from .typing import *
--- pandas_dataclasses/pandas/__init__.py
+++ pandas_dataclasses/pandas/__init__.py
@@ -1,8 +1,3 @@
 # submodules
-from . import frame  # type: ignore
-from . import series  # type: ignore
-
-
-# aliases
 from .frame import *
 from .series import *
