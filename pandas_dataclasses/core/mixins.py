3d8a0a78a7f2ce612d1516428937b30a4610af50
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-10-23 16:38:07 +0000

    #96 Update mixins module (refactoring)

--- pandas_dataclasses/core/mixins.py
+++ pandas_dataclasses/core/mixins.py
@@ -5,7 +5,7 @@ __all__ = ["As", "AsDataFrame", "AsSeries"]
 from copy import copy
 from functools import wraps as wraps_
 from types import FunctionType, MethodType
-from typing import Any, Callable, ForwardRef, Generic, Type, cast
+from typing import Any, Callable, ForwardRef, Generic, Type
 
 
 # dependencies
@@ -15,7 +15,7 @@ from typing_extensions import get_args, get_origin
 
 # submodules
 from .asdata import asdataframe, asseries
-from .typing import P, T, PandasClass, TPandas
+from .typing import P, T, Pandas, PandasClass, TPandas
 
 
 class classproperty:
@@ -34,19 +34,20 @@ class classproperty:
 
 
 class As(Generic[TPandas]):
-    """Mix-in class that provides shorthand methods."""
+    """Mix-in class for runtime pandas data creator."""
 
     __pandas_factory__: Callable[..., TPandas]
     """Factory for pandas data creation."""
 
     def __init_subclass__(cls, **kwargs: Any) -> None:
         """Add a pandas factory to an inheriting class."""
-        cls.__pandas_factory__ = kwargs.pop("factory", get_factory(cls))
+        factory = kwargs.pop("factory", None)
+        cls.__pandas_factory__ = factory or get_factory(cls)
         super().__init_subclass__(**kwargs)
 
     @classproperty
     def new(cls) -> MethodType:
-        """Create a pandas object from dataclass parameters."""
+        """Runtime pandas data creator as a classmethod."""
         return MethodType(get_new(cls), cls)
 
 
@@ -70,12 +71,12 @@ def get_factory(cls: Any) -> Callable[..., Any]:
         if factory == ForwardRef("pd.Series[Any]"):
             return pd.Series
 
-        return cast(Callable[..., Any], factory)
+        return factory  # type: ignore
 
     raise TypeError("Could not find any factory.")
 
 
-def get_new(cls: Any) -> Callable[..., Any]:
+def get_new(cls: Any) -> Callable[..., Pandas]:
     """Create a runtime new function from a class."""
     factory = cls.__pandas_factory__
     origin = get_origin(factory) or factory
@@ -88,10 +89,10 @@ def get_new(cls: Any) -> Callable[..., Any]:
         raise TypeError("Could not choose a converter.")
 
     @wraps(cls.__init__, "new", factory)
-    def wrapper(cls: Any, *args: Any, **kwargs: Any) -> Any:
+    def new(cls: Any, *args: Any, **kwargs: Any) -> Any:
         return converter(cls(*args, **kwargs))
 
-    return wrapper
+    return new
 
 
 def wraps(func: Any, name: str, return_: Any) -> Callable[[T], T]:
