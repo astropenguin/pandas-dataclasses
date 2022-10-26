2e03d7228f7867d2c5efe265ec2d0382e375f8cb
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-10-26 11:59:15 +0000

    #108 Add aliases for renamed items

--- pandas_dataclasses/__init__.py
+++ pandas_dataclasses/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     "As",
+    "AsDataFrame",
     "AsFrame",
     "AsSeries",
     "Attr",
@@ -8,6 +9,7 @@ __all__ = [
     "Index",
     "Other",
     "Spec",
+    "asdataframe",
     "asframe",
     "asseries",
     "core",
@@ -23,3 +25,12 @@ from .core.typing import *
 
 # metadata
 __version__ = "0.8.0"
+
+
+# aliases
+AsDataFrame = AsFrame
+"""Alias of ``core.mixins.AsFrame``."""
+
+
+asdataframe = asframe
+"""Alias of ``core.aspandas.asframe``."""
