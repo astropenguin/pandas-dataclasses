275ca130f3cbf64039268e308709290b2a5474b7
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-10-26 11:51:41 +0000

    #108 Update test for mixins module

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -15,47 +15,47 @@ def factory(*args: Any, **kwargs: Any) -> pd.Series:  # type: ignore
     return pd.Series(*args, **kwargs)  # type: ignore
 
 
-class CustomDataFrame(pd.DataFrame):
+class UserFrame(pd.DataFrame):
     pass
 
 
-class CustomSeries(pd.Series):  # type: ignore
-    pass
-
-
-@dataclass
-class DataFrameWeather(Weather, AsFrame):
+class UserSeries(pd.Series):  # type: ignore
     pass
 
 
 @dataclass
-class CustomDataFrameWeather(Weather, As[CustomDataFrame]):
+class Frame(Weather, AsFrame):
     pass
 
 
 @dataclass
-class SeriesWeather(Weather, AsSeries):
+class CustomFrame(Weather, As[UserFrame]):
     pass
 
 
 @dataclass
-class FactorySeriesWeather(Weather, AsSeries, factory=factory):
+class Series(Weather, AsSeries):
     pass
 
 
 @dataclass
-class CustomSeriesWeather(Weather, As[CustomSeries]):
+class CustomSeries(Weather, As[UserSeries]):
     pass
 
 
 @dataclass
-class FloatSeriesWeather(Weather, As["pd.Series[float]"], factory=pd.Series):
+class FactorySeries(Weather, AsSeries, factory=factory):
+    pass
+
+
+@dataclass
+class FloatSeries(Weather, As["pd.Series[float]"], factory=pd.Series):
     pass
 
 
 # test functions
-def test_dataframe_weather() -> None:
-    df_weather = DataFrameWeather.new(
+def test_frame() -> None:
+    df_weather = Frame.new(
         year=weather.year,
         month=weather.month,
         temp_avg=weather.temp_avg,
@@ -68,8 +68,8 @@ def test_dataframe_weather() -> None:
     assert_frame_equal(df_weather, df_weather_true)
 
 
-def test_custom_dataframe_weather() -> None:
-    df_weather = CustomDataFrameWeather.new(
+def test_custom_frame() -> None:
+    df_weather = CustomFrame.new(
         year=weather.year,
         month=weather.month,
         temp_avg=weather.temp_avg,
@@ -78,12 +78,12 @@ def test_custom_dataframe_weather() -> None:
         wind_max=weather.wind_max,
     )
 
-    assert isinstance(df_weather, CustomDataFrame)
+    assert isinstance(df_weather, UserFrame)
     assert_frame_equal(df_weather, df_weather_true, check_frame_type=False)
 
 
-def test_series_weather() -> None:
-    ser_weather = SeriesWeather.new(
+def test_series() -> None:
+    ser_weather = Series.new(
         year=weather.year,
         month=weather.month,
         temp_avg=weather.temp_avg,
@@ -96,8 +96,8 @@ def test_series_weather() -> None:
     assert_series_equal(ser_weather, ser_weather_true)
 
 
-def test_factory_series_weather() -> None:
-    ser_weather = FactorySeriesWeather.new(
+def test_custom_series() -> None:
+    ser_weather = CustomSeries.new(
         year=weather.year,
         month=weather.month,
         temp_avg=weather.temp_avg,
@@ -106,26 +106,26 @@ def test_factory_series_weather() -> None:
         wind_max=weather.wind_max,
     )
 
-    assert isinstance(ser_weather, pd.Series)
-    assert_series_equal(ser_weather, ser_weather_true)
-
-
-def test_custom_series_weather() -> None:
-    ser_weather = CustomSeriesWeather.new(
-        year=weather.year,
-        month=weather.month,
-        temp_avg=weather.temp_avg,
-        temp_max=weather.temp_max,
-        wind_avg=weather.wind_avg,
-        wind_max=weather.wind_max,
-    )
-
-    assert isinstance(ser_weather, CustomSeries)
+    assert isinstance(ser_weather, UserSeries)
     assert_series_equal(ser_weather, ser_weather_true, check_series_type=False)
 
 
-def test_float_series_weather() -> None:
-    ser_weather = FloatSeriesWeather.new(
+def test_factory_series() -> None:
+    ser_weather = FactorySeries.new(
+        year=weather.year,
+        month=weather.month,
+        temp_avg=weather.temp_avg,
+        temp_max=weather.temp_max,
+        wind_avg=weather.wind_avg,
+        wind_max=weather.wind_max,
+    )
+
+    assert isinstance(ser_weather, pd.Series)
+    assert_series_equal(ser_weather, ser_weather_true)
+
+
+def test_float_series() -> None:
+    ser_weather = FloatSeries.new(
         year=weather.year,
         month=weather.month,
         temp_avg=weather.temp_avg,
